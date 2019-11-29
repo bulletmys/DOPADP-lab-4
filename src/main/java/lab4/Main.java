370319d465a22266cfc510a08b6f61719c515f16
bulletmys <bulletmys@yandex.ru>
2019-11-30 02:51:36 +0300

    Scripted auto-commit on change (2019-11-30 02:51:36) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         Http http = Http.get(system);
         ActorMaterializer actorMaterializer =  ActorMaterializer.create(system);
-        Http
+        
 
         return 0;
     }
