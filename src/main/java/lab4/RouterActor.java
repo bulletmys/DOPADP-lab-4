cd24f952d1ffe8fd9cfc7b2542e99be479e25ce4
bulletmys <bulletmys@yandex.ru>
2019-11-30 00:50:29 +0300

    Scripted auto-commit on change (2019-11-30 00:50:29) by gitwatch.sh

--- src/main/java/lab4/RouterActor.java
+++ src/main/java/lab4/RouterActor.java
@@ -2,11 +2,12 @@ package lab4;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.routing.RoundRobinPool;
 
 public class RouterActor extends AbstractActor {
 
     private ActorRef execActor = getContext().actorOf(TestKeeperActor.props());
-    private 
+    private ActorRef testKeeperPool = getContext().actorOf(new RoundRobinPool(1))
 
     @Override
     public Receive createReceive() {
