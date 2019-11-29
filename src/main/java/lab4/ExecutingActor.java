f3d922c0973ad72eb8d61a89c62e42295cfabca2
bulletmys <bulletmys@yandex.ru>
2019-11-30 00:19:18 +0300

    Scripted auto-commit on change (2019-11-30 00:19:18) by gitwatch.sh

--- src/main/java/lab4/ExecutingActor.java
+++ src/main/java/lab4/ExecutingActor.java
@@ -3,6 +3,7 @@ package lab4;
 import akka.actor.AbstractActor;
 
 public class ExecutingActor extends AbstractActor {
+    
     @Override
     public Receive createReceive() {
         return null;
