299f9b5a1c1523f20631960a78dd056549a5d13f
bulletmys <bulletmys@yandex.ru>
2019-11-30 02:01:35 +0300

    Scripted auto-commit on change (2019-11-30 02:01:35) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -1,11 +1,12 @@
 package lab4;
 
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
 public class Main {
     int main(String[] args) {
         ActorSystem system = ActorSystem.create("test-system");
-
+        ActorRef
     }
 }
