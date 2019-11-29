c2066c03df3c620a58187f7c6a97f3740f5ebea1
bulletmys <bulletmys@yandex.ru>
2019-11-30 00:42:12 +0300

    Scripted auto-commit on change (2019-11-30 00:42:12) by gitwatch.sh

--- src/main/java/lab4/TestKeeperActor.java
+++ src/main/java/lab4/TestKeeperActor.java
@@ -1,11 +1,16 @@
 package lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.Props;
 
 public class TestKeeperActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
         return null;
+    }
+
+    public static Props props() {
+        return Props.create(ExecutingActor.class);
     }
 }
