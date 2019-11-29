56db1a85755bba57309263af989c4370fb920ac2
bulletmys <bulletmys@yandex.ru>
2019-11-30 01:56:26 +0300

    Scripted auto-commit on change (2019-11-30 01:56:26) by gitwatch.sh

--- src/main/java/lab4/TestMessage.java
+++ src/main/java/lab4/TestMessage.java
@@ -8,7 +8,26 @@ public class TestMessage {
     String funcName;
     ArrayList<TestUnit> tests;
 
-    TestMessage(int packageID, String script, String funcName) {
+    TestMessage(int packageID, String script, String funcName, ArrayList<TestUnit> tests) {
+        this.packageID = packageID;
+        this.script = script;
+        this.funcName = funcName;
+        this.tests = tests;
+    }
 
+    public ArrayList<TestUnit> getTests() {
+        return tests;
+    }
+
+    public int getPackageID() {
+        return packageID;
+    }
+
+    public String getFuncName() {
+        return funcName;
+    }
+
+    public String getScript() {
+        return script;
     }
 }
