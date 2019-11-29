4bbe73c4e1670a39de311c93d8e571486e12db2e
bulletmys <bulletmys@yandex.ru>
2019-11-30 01:56:55 +0300

    Scripted auto-commit on change (2019-11-30 01:56:55) by gitwatch.sh

--- src/main/java/lab4/ExecutingActor.java
+++ src/main/java/lab4/ExecutingActor.java
@@ -3,13 +3,21 @@ package lab4;
 import akka.actor.AbstractActor;
 import akka.actor.Props;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class ExecutingActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(TestMessage.class, mail -> {
-                    
+                    ScriptEngine engine = new
+                            ScriptEngineManager().getEngineByName("nashorn");
+                    engine.eval(jscript);
+                    Invocable invocable = (Invocable) engine;
+                    return invocable.invokeFunction(functionName, params).toString();
                 })
     }
 
