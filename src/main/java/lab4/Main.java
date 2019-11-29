3c1978e1a2cff2c4c40f40f9bf4705f8d81d90db
bulletmys <bulletmys@yandex.ru>
2019-11-30 02:49:17 +0300

    Scripted auto-commit on change (2019-11-30 02:49:17) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -8,7 +8,8 @@ public class Main {
     int main(String[] args) {
         ActorSystem system = ActorSystem.create("test-system");
         ActorRef routeActor = system.actorOf(RouterActor.props(), "routeActor");
-        
+
+        Htt
 
         return 0;
     }
