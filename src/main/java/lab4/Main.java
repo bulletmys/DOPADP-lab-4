fe241470e830b9f00531208597fdcd1dc6053504
bulletmys <bulletmys@yandex.ru>
2019-11-30 03:51:06 +0300

    Scripted auto-commit on change (2019-11-30 03:51:06) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -4,10 +4,9 @@ package lab4;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
-import akka.http.javadsl.server.AllDirectives;
 import akka.stream.ActorMaterializer;
 
-public class Main extends AllDirectives {
+public class Main {
     int main(String[] args) {
         ActorSystem system = ActorSystem.create("test-system");
         ActorRef routeActor = system.actorOf(RouterActor.props(), "routeActor");
