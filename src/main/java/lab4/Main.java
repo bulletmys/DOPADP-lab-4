141c9d229af5b964019c3cb9ba6aff396a614f4a
bulletmys <bulletmys@yandex.ru>
2019-11-30 04:09:07 +0300

    Scripted auto-commit on change (2019-11-30 04:09:07) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -5,6 +5,10 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
     int main(String[] args) {
@@ -14,7 +18,7 @@ public class Main {
         Http http = Http.get(system);
         ActorMaterializer actorMaterializer =  ActorMaterializer.create(system);
 
-        final Flow<>
+        final Flow<HttpRequest, HttpResponse>
 
 
         return 0;
