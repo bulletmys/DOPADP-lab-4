1c1dd65aaf5895a1a2e487110f944be5ef9b147e
bulletmys <bulletmys@yandex.ru>
2019-11-30 04:05:37 +0300

    Scripted auto-commit on change (2019-11-30 04:05:37) by gitwatch.sh

--- src/main/java/lab4/Router.java
+++ src/main/java/lab4/Router.java
@@ -18,7 +18,9 @@ public class Router extends AllDirectives {
                                 })
                         )
                 ),
-                path(parameter("packageID", () ->packageID -> {
+                path(parameter("packageID", () ->
+                        get() ->
+        packageID -> {
                             Future<Object> res = Patterns.ask(routerActor, new GetTestsResult(Integer.parseInt(packageID)), 1000);
                             return completeOKWithFuture(res, Jackson.marshaller());
                         })
