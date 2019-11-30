e7365236498b7dbb7443316cd1d58515819602f5
bulletmys <bulletmys@yandex.ru>
2019-11-30 03:52:57 +0300

    Scripted auto-commit on change (2019-11-30 03:52:57) by gitwatch.sh

--- src/main/java/lab4/Router.java
+++ src/main/java/lab4/Router.java
@@ -5,5 +5,7 @@ import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 
 public class Router extends AllDirectives {
-    Route createRoute(ActorRef )
+    Route createRoute(ActorRef routerActor) {
+        
+    }
 }
