1b390d387225a61fa74a86dbeae24b5599adebc1
bulletmys <bulletmys@yandex.ru>
2019-11-30 03:59:10 +0300

    Scripted auto-commit on change (2019-11-30 03:59:10) by gitwatch.sh

--- src/main/java/lab4/Router.java
+++ src/main/java/lab4/Router.java
@@ -16,7 +16,9 @@ public class Router extends AllDirectives {
                                 })
                         )
                 ),
-                path()
+                path(parameter(p)
+
+                )
         )
     }
 }
